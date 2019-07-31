0a7b999a86752d7148212660de3016d909af2516
AtkinsJM <github@joshatkins.co.uk>
2019-07-31 21:12:28 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MedievalMayhem
+
+Developed with Unreal Engine 4
---
+++ Source/MedievalMayhem/MedievalMayhemGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MedievalMayhemGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MEDIEVALMAYHEM_API AMedievalMayhemGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
