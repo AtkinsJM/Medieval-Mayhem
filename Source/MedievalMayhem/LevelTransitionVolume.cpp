8ae5a36346e72bad0593d21ffaa3f7cc7bb5e82b
AtkinsJM <github@joshatkins.co.uk>
2019-12-12 23:22:12 +0000

    Created and implemented LevelTransitionVolume class.

---
+++ Source/MedievalMayhem/LevelTransitionVolume.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LevelTransitionVolume.h"
+#include "Kismet/GameplayStatics.h"
+#include "MainCharacter.h"
+
+// Sets default values
+ALevelTransitionVolume::ALevelTransitionVolume()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	TransitionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("Transition Box"));
+}
+
+// Called when the game starts or when spawned
+void ALevelTransitionVolume::BeginPlay()
+{
+	Super::BeginPlay();
+	TransitionBox->OnComponentBeginOverlap.AddDynamic(this, &ALevelTransitionVolume::OnBeginOverlap);
+}
+
+void ALevelTransitionVolume::OnBeginOverlap(UPrimitiveComponent * OverlappedComponent, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
+{
+	AMainCharacter* MainCharacter = Cast<AMainCharacter>(OtherActor);
+	if (MainCharacter)
+	{
+		LoadLevel();
+	}
+}
+
+void ALevelTransitionVolume::LoadLevel()
+{
+	if (LevelToLoad != "")
+	{
+		UGameplayStatics::OpenLevel(GetWorld(), LevelToLoad);
+		//FLatentActionInfo LatentInfo;
+		//UGameplayStatics::LoadStreamLevel(this, LevelToLoad, true, true, LatentInfo);
+	}
+}
+
+
